e1162d12d9bba7309b4c7eddd5e9c078d4872a96
Yan Ke <yansongke1@gmail.com>
2016-12-18 00:46:04 -0500

    edit about

--- app/admins.py
+++
@@ -1,9 +0,0 @@
-from app import admin, db
-from .models import Intro, Projects, ProjectPic, User, About
-from .security import ModelView
-
-admin.add_view(ModelView(Intro, db.session))
-admin.add_view(ModelView(Projects, db.session))
-admin.add_view(ModelView(ProjectPic, db.session))
-admin.add_view(ModelView(About, db.session))
-admin.add_view(ModelView(User, db.session))
--- app/views.py
+++
@@ -1,8 +0,0 @@
-from app import app
-from flask import render_template, url_for
-
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
-
