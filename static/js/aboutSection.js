b5a027e7ffc5d9626082d4ff9116fb76d6759e3e
Yan Ke <yansongke1@gmail.com>
2016-12-19 23:38:19 -0500

    about tabs

---
+++ static/js/aboutSection.js
@@ -0,0 +1,4 @@
+var height = document.getElementById('about').style.height;
+var width = document.getElementByID('about').style.width;
+document.getElementById("divHeight").innerHTML = 29;
+document.getElementById("divWidth").innerHTML = width;
