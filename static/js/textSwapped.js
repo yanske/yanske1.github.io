c01f6eee27babebad553db79b58cd0684a86cbf9
yanske1 <yansongke1@gmail.com>
2017-03-10 12:06:06 -0500

    fixed text change bug

--- static/js/textSwapped.js
+++ static/js/textSwapped.js
@@ -1,31 +1,11 @@
-var greetings = ["Hoi", "Hey", "Hello", "Heyo"];
+var greetings = ["Hoi,", "Hey,", "Hello,", "Heyo,"];
 var index = 0;
-var firstTime = true;
 var totalDelay = 3300;
 
 window.setInterval(function(){
-	
-	if(firstTime == false)
-	{
-		$("#greet").fadeIn(400).delay(2500).fadeOut(400);
-	}
-	else if(firstTime == true)
-	{
-		$("#greet").delay(2900).fadeOut(400);
-		firstTime = false;
-	}
-	
-  	var greet = document.getElementById('greet').innerHTML;
-  	var next = index+1;
-  	next=next%4;
-	var res = greet.replace(greetings[index], greetings[next]);
-
-	document.getElementById('greet').innerHTML = res;
-	index = next;
-
+	$("#greet").fadeOut(function(){
+		$("#greet").html(greetings[index]).fadeIn();
+	});
+	if(++index >= 4) index -= 4;
 }, totalDelay);
-
-
-
-
 
