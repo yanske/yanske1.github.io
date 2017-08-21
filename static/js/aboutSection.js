06a590f7eba20b9cdefbc53fa05ccea2508879e0
yanske1 <yansongke1@gmail.com>
2017-08-21 14:11:13 -0400

    flicker effect

--- static/js/aboutSection.js
+++ static/js/aboutSection.js
@@ -3,3 +3,15 @@ var app = angular.module("myApp", []);
 	$scope.colorHover= "#b7b6b3";
 	$scope.notHover= "#a2a09c";
 });
+
+$(document).ready(function(){
+	setInterval('cursorAnimation()', 600);
+});
+
+function cursorAnimation(){
+	$('#cursor').animate({
+		opacity: 0
+	}, 'fast', 'swing').animate({
+		opacity: 1
+	}, 'fast', 'swing');
+}
