3e6997a65748c85e9f0c09b6261e3ea2e0a57d54
yanske1 <yansongke1@gmail.com>
2018-08-23 11:33:48 -0400

    mobile dropdown

--- assets/js/menuToggle.js
+++ assets/js/menuToggle.js
@@ -1,7 +1,11 @@
 window.onload = function() {
-  var trigger = document.getElementById("dropdown-trigger");
-  trigger.addEventListener("touchstart", function(event){
-    trigger.checked = !trigger.checked;
+  var trigger = document.getElementsByClassName("dropdown-trigger")[0];
+  var mobileDropdown = document.getElementsByClassName("mobile-dropdown")[0];
+  var active = false;
+
+  trigger.addEventListener("click", function(event){
+    mobileDropdown.style.height = active ? 0 : "auto";
+    active = !active;
     event.preventDefault();
   });
 }
