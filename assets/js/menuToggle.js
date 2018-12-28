20ff4a246776fe46785d989b579e9c5227e182d4
Yan <yansongke1@gmail.com>
2018-12-27 23:34:06 -0500

    V1 remove header

--- assets/js/menuToggle.js
+++
@@ -1,9 +0,0 @@
-var trigger = document.getElementsByClassName("dropdown-trigger")[0];
-var mobileDropdown = document.getElementsByClassName("mobile-dropdown")[0];
-var active = false;
-
-trigger.addEventListener("click", function(event){
-  mobileDropdown.style.height = active ? 0 : "auto";
-  active = !active;
-  event.preventDefault();
-});
